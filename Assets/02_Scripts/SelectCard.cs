e612d407dcae2ff640552296c54d0f746997d141
00CHI <suinmoon83@gmail.com>
2025-10-15 16:00:54 +0900

    251015_PM3:59
    - SelectCard 카드 하나 선택 구현
    - SelectCard Canvas 애니메이션 구현
    
    ** SelectCard 카드 하나 선택+ SelectCard Canvas 애니메이션 합치기
    ** 카드 뒤집기
    ** 나왔던 질문 안나오게 구현
    <= 인덱스 문제 해결 필요

--- Assets/02_Scripts/SelectCard.cs
+++ Assets/02_Scripts/SelectCard.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using UnityEngine.UI;
 //using UnityEngine.UIElements;
 
-public class SelectCard : MonoBehaviour
+class SelectCard : SpreadCard
 {
     [SerializeField]
     GameObject studentCanvas;
@@ -16,8 +16,7 @@ public class SelectCard : MonoBehaviour
     [SerializeField]
     GameObject crimeCanvas;
 
-
-    public Button studentButton;
+    Button studentButton;
     Button timeButton;
     Button crimeButton;
 
@@ -25,42 +24,35 @@ public class SelectCard : MonoBehaviour
     Button[] timeButtons;
     Button[] crimeButtons;
 
-
-    [Header("Card Settings")]
-    public GameObject cardPrefab;   // 카드 프리팹
-    public int rows = 2;            // 행 개수
-    public int columns = 4;         // 열 개수
-    public float spacingX = 150f;   // 카드 간 가로 간격
-    public float spacingY = 200f;   // 카드 간 세로 간격
-    public float appearDuration = 0.3f; // 카드가 등장하는 시간 (애니메이션)
-
-    private List<GameObject> cards = new List<GameObject>();
-
     // Start is called before the first frame update
     void Start()
     {
         SelectAllButtons();
+        SetCard(studentCanvas);
+
 
         studentButton.onClick.AddListener(SelectStudentCard);
         timeButton.onClick.AddListener(SelectTimeCard);
         crimeButton.onClick.AddListener(SelectCrimeCard);
 
-        CreateCards();
-        StartCoroutine(SpreadCards());
     }
 
     //Student => Time => Crime
     void SelectStudentCard()
     {
         SetFalseCard(timeCanvas, studentCanvas, crimeCanvas);
+        SetCard(timeCanvas);
+        
     }
     void SelectTimeCard()
     {
         SetFalseCard(crimeCanvas, studentCanvas, timeCanvas);
+        SetCard(crimeCanvas);
+
     }
     void SelectCrimeCard()
     {
-        crimeCanvas.SetActive(false);
+        gameObject.SetActive(false);
     }
 
     void SelectAllButtons()
@@ -81,10 +73,10 @@ public class SelectCard : MonoBehaviour
             return null;
         }
 
-        int _buttonIndex = Random.Range(0, _ALLBUTTONS.Length);
+        int _buttonIndex = UnityEngine.Random.Range(0, _ALLBUTTONS.Length);
         Button _selectedButton = _ALLBUTTONS[_buttonIndex];
-        
-        for(int i = 0; i< _ALLBUTTONS.Length; i++)
+
+        for (int i = 0; i < _ALLBUTTONS.Length; i++)
         {
             if (i == _buttonIndex)
             {
@@ -92,76 +84,17 @@ public class SelectCard : MonoBehaviour
             }
             else
             {
-
+                _ALLBUTTONS[i].interactable = false;
             }
         }
 
         return _selectedButton;
-
-
     }
-
 
     void SetFalseCard(GameObject _CANVAS1, GameObject _CANVAS2, GameObject _CANVAS3)
     {
         _CANVAS1.SetActive(true);
         _CANVAS2.SetActive(false);
         _CANVAS3.SetActive(false);
-    }
-
-    void CreateCards()
-    {
-        int _total = rows * columns;
-
-        for (int i = 0; i < _total; i++)
-        {
-            GameObject _card = Instantiate(cardPrefab, transform);
-            _card.transform.localScale = Vector3.zero; // 처음에는 0으로 숨김
-            cards.Add(_card);
-        }
-    }
-
-    IEnumerator SpreadCards()
-    {
-        // 중심 기준으로 카드 정렬
-        float _startX = -(columns - 1) * spacingX / 2f;
-        float _startY = (rows - 1) * spacingY / 2f;
-
-        int _index = 0;
-        for (int y = 0; y < rows; y++)
-        {
-            for (int x = 0; x < columns; x++)
-            {
-                if (_index >= cards.Count)
-                    yield break;
-
-                GameObject card = cards[_index];
-                Vector3 targetPos = new Vector3(_startX + (x * spacingX), _startY - (y * spacingY), 0);
-                StartCoroutine(AnimateCard(card.transform, targetPos, appearDuration));
-
-                _index++;
-                yield return new WaitForSeconds(0.05f); // 한 장씩 순차적으로 나오는 효과
-            }
-        }
-    }
-
-    IEnumerator AnimateCard(Transform _CARD, Vector3 _TARGETPOS, float _DURATION)
-    {
-        Vector3 _startPos = Vector3.zero;
-        Vector3 _startScale = Vector3.zero;
-        Vector3 _endScale = Vector3.one;
-
-        float _t = 0f;
-        while (_t < _DURATION)
-        {
-            _t += Time.deltaTime;
-            float _progress = Mathf.SmoothStep(0, 1, _t / _DURATION);
-            _CARD.localPosition = Vector3.Lerp(_startPos, _TARGETPOS, _progress);
-            _CARD.localScale = Vector3.Lerp(_startScale, _endScale, _progress);
-            yield return null;
-        }
-
-        _CARD.localPosition = _TARGETPOS;
-        _CARD.localScale = _endScale;
     }
 }
---
+++ Assets/02_Scripts/SpreadCard.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SpreadCard : MonoBehaviour
+{
+    [Header("Card Settings")]
+     RectTransform startPos;
+     int rows = 2;             // 행 개수
+     int columns = 6;          // 열 개수
+     float spacingX = 290f;    // 카드 간 가로 간격
+     float spacingY = 470f;    // 카드 간 세로 간격
+     float moveDuration = 0.3f; // 카드 등장 애니메이션 시간
+
+    public List<Transform> cards = new List<Transform>();
+
+    protected void SetCard(GameObject _PARENTPOS)
+    {
+
+        foreach (Transform child in _PARENTPOS.transform)
+        {
+            cards.Add(child);
+
+            startPos = cards[0].GetComponent<RectTransform>();
+        }
+
+        if (startPos != null)
+        {
+            foreach (RectTransform cardsPos in cards)
+            {
+                cardsPos.anchoredPosition = startPos.anchoredPosition;
+                cardsPos.localScale = Vector3.zero;
+            }
+        }
+
+        StartCoroutine(SpreadCards());
+    }
+
+    IEnumerator SpreadCards()
+    {
+        if (startPos == null)
+        {
+            Debug.LogWarning("StartPoint가 설정되지 않았습니다!");
+            yield break;
+        }
+
+        //RectTransform _startPos = startPos.GetComponent<RectTransform>();
+
+        int _cardIndex = 0;
+
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < columns; x++)
+            {
+                if(_cardIndex >= cards.Count)
+                {
+                    yield break;
+                }
+
+               RectTransform _card = cards[_cardIndex].GetComponent<RectTransform>();
+
+                yield return new WaitForSeconds(0.2f);
+
+                Vector2 _targetPos = new Vector2(
+                    startPos.anchoredPosition.x + (x * spacingX),
+                    startPos.anchoredPosition.y - (y * spacingY)        
+                );
+
+                StartCoroutine(AnimateCard(_card, _targetPos, moveDuration));
+                _cardIndex++;
+
+
+            }
+        }
+    }
+
+    IEnumerator AnimateCard(RectTransform _CARD, Vector3 _TARGETPOS, float _DURATION)
+    {
+        //Vector3 _startPos = Vector3.zero;
+        Vector3 _startPos = new Vector3(_CARD.anchoredPosition.x, _CARD.anchoredPosition.y);
+        Vector3 _startScale = Vector3.zero;
+        Vector3 _endScale = Vector3.one;
+
+        float t = 0f;
+        while (t < _DURATION)
+        {
+            t += Time.deltaTime;
+            float _progress = Mathf.SmoothStep(0, 1, t / _DURATION);
+            _CARD.anchoredPosition = Vector3.Lerp(_startPos, _TARGETPOS, _progress);
+            _CARD.localScale = Vector3.Lerp(_startScale, _endScale, _progress);
+            yield return null;
+
+        }
+
+        //_CARD.localPosition = _TARGETPOS;
+        _CARD.localScale = _endScale;
+
+
+    }
+}
