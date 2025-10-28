f279634e614852f9e70e7cf08133ad41128a2cbb
00CHI <167988237+00CHI@users.noreply.github.com>
2025-10-28 18:22:07 +0900

    251028_PM 6:20
    -카드 뒤집기 애니메이션
    -카드 선택 후 기다렸다 사라지도록 구현
    
    - 카드 랜덤하게 선택되도록 구현
    
    ** 나왔던 질문 안나오게 구현
    <= 인덱스 문제 해결 필요
    ** 선택한 카드 정보 저장
    및 하단 카드 상태바에 띄우기

--- Assets/02_Scripts/SelectCard.cs
+++ Assets/02_Scripts/SelectCard.cs
@@ -1,11 +1,12 @@
-//using System;
+Ôªøusing DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using Unity.Collections.LowLevel.Unsafe;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 //using UnityEngine.UIElements;
 
 class SelectCard : SpreadCard
@@ -17,12 +18,10 @@ class SelectCard : SpreadCard
     [SerializeField]
     GameObject crimeCanvas;
 
-
-
-    Button studentButton;
-    Button timeButton;
-    Button crimeButton;
-    Button selectedButton;
+    public Button studentButton;
+    public Button timeButton;
+    public Button crimeButton;
+    public Button selectedButton;
 
 
     Button[] studentButtons;
@@ -38,113 +37,216 @@ class SelectCard : SpreadCard
 
     Ease ease = Ease.InOutSine;
 
+    List<string> studentNames = new List<string>()
+    {
+        "Ari_Choi",//1
+        "bada_Seo",//2
+        "Bora_Nam",//3
+        "doha_Bae",//4
+        "Donghoon_Moon",//5
+        "galam_Heo",//6
+        "hali_Gu",//7
+        "hanbyeol_Bu",//8
+        "Jiho_Lee",//9
+        "mingug_Jo",//10
+        "Minjae_Kim",//11
+        "Mirae_Yoon",//12
+        "schoolmaste"//13
+    };
+
+    List<string> timeNames = new List<string>()
+    {
+        "After",//1
+        "Break",//2
+        "CleaningTime",//3
+        "DropOff",//4
+        "GoToSchool",//5
+        "Lunch",//6
+    };
+
+    List<string> crimeNames = new List<string>()
+    {
+        "Absence",//1
+        "Alcohol",//2
+        "Cigarette",//3
+        "Cleaning",//4
+        "Doodle",//5
+        "Dues",//6
+        "Homework",//7
+        "InformalLanguage",//8
+        "Restroom",//9
+        "Teacher",//10
+        "Test",//11
+        "Washing",//12
+    };
+
     // Start is called before the first frame update
     void Start()
     {
-        studentButton = SelectButton(studentCanvas, studentButtons);
-
         selectedCardPos = slectedCard.anchoredPosition;
+
 
         SetCard(studentCanvas);
 
-        studentButton.onClick.AddListener(SelectStudentCard);
-        //timeButton.onClick.AddListener(SelectTimeCard);
-        //crimeButton.onClick.AddListener(SelectCrimeCard);
+        SelectButton(studentCanvas, ref studentButtons, (idx) => SelectStudentCard(idx));
     }
-
 
 
     //Student => Time => Crime    
-    void SelectStudentCard()
+    void SelectStudentCard(int index)
+    {
+        selectedButton = studentButtons[index];
+        studentButton = studentButtons[index];
+
+        int _nameIndex = UnityEngine.Random.Range(0, studentNames.Count);//{_studentName}
+        string _studentName = studentNames[_nameIndex];
+
+        CardRotate(selectedButton.transform, "01_studentID", $"{_studentName}", "studentID_back", timeCanvas, studentCanvas, crimeCanvas);//, timeCanvas, studentCanvas, crimeCanvas, studentButton
+
+
+        SelectButton(timeCanvas, ref timeButtons, (idx) => SelectTimeCard(idx));
+
+    }
+
+    void SelectTimeCard(int index)
+    {
+        selectedButton = timeButtons[index];
+        timeButton = timeButtons[index];
+
+        if (timeButton == null)
+        {
+            Debug.Log("None Timebutton");
+        }
+
+        int _nameIndex = UnityEngine.Random.Range(0, timeNames.Count);//{_studentName}
+        string _timeName = timeNames[_nameIndex];
+
+        CardRotate(selectedButton.transform, "02_time", $"{_timeName}", "time_back", crimeCanvas, studentCanvas, timeCanvas);//, crimeCanvas, studentCanvas, timeCanvas, timeButton
+
+        SelectButton(crimeCanvas, ref crimeButtons, (idx) => SelectCrimeCard(idx));
+
+    }
+    void SelectCrimeCard(int index)
     {
 
-        CatdRotate(selectedButton.transform, "01_studentID", "Ari_Choi", "studentID_back");
+        selectedButton = crimeButtons[index];
+        crimeButton = crimeButtons[index];
 
-        //timeButton = SelectButton(timeCanvas, timeButtons);
+        int _nameIndex = UnityEngine.Random.Range(0, crimeNames.Count);//{_studentName}
+        string _crimeName = crimeNames[_nameIndex];
 
-        //SetFalseCard(timeCanvas, studentCanvas, crimeCanvas);
-        //cards.Clear();
-        //SetCard(timeCanvas);
+        if (crimeButton == null)
+        {
+            Debug.Log("None Crimebutton");
+        }
+
+        CardRotate(selectedButton.transform, "03_crime", $"{_crimeName}", "crime_back", crimeCanvas, studentCanvas, timeCanvas);//, crimeCanvas, studentCanvas, timeCanvas, crimeButton
+
+
+        DOVirtual.DelayedCall(2.5f, () => SetActiveFalse());
     }
-    void SelectTimeCard()
-    {
-        SetFalseCard(crimeCanvas, studentCanvas, timeCanvas);
-        crimeButton = SelectButton(crimeCanvas, crimeButtons);
 
-        cards.Clear();
-        SetCard(crimeCanvas);
-    }
-    void SelectCrimeCard()
+    void SetActiveFalse()
     {
         gameObject.SetActive(false);
     }
 
-
-    void SetFalseCard(GameObject _CANVAS1, GameObject _CANVAS2, GameObject _CANVAS3)
-    {
-        _CANVAS1.SetActive(true);
-        _CANVAS2.SetActive(false);
-        _CANVAS3.SetActive(false);
-    }
-
-
-    Button SelectButton(GameObject _PARENTCANVAS, Button[] _ALLBUTTONS)
+    void SelectButton(GameObject _PARENTCANVAS,ref Button[] _ALLBUTTONS, Action<int> _ONCLICKED)
     {
         _ALLBUTTONS = _PARENTCANVAS.GetComponentsInChildren<Button>();
 
         if (_ALLBUTTONS == null || _ALLBUTTONS.Length == 0)
         {
-            Debug.LogWarning($"{_PARENTCANVAS.name} æ»ø° Button¿Ã æ¯Ω¿¥œ¥Ÿ!");
-            return null;
+            Debug.LogWarning($"{_PARENTCANVAS.name} ÏïàÏóê ButtonÏù¥ ÏóÜÏäµÎãàÎã§!");
+
+            return;
         }
 
-        int _buttonIndex = UnityEngine.Random.Range(0, _ALLBUTTONS.Length);
-        selectedButton = _ALLBUTTONS[_buttonIndex];
 
         for (int i = 0; i < _ALLBUTTONS.Length; i++)
         {
-            if (i == _buttonIndex)
-            {
-                _ALLBUTTONS[i].interactable = true; // ≈¨∏Ø ∞°¥…
-            }
-            else
-            {
-                _ALLBUTTONS[i].interactable = false;
-            }
+            int index = i;
+
+            _ALLBUTTONS[i].onClick.AddListener(() => _ONCLICKED(index));
         }
 
-        cardRenderer = selectedButton.GetComponent<Image>();
-
-
-
-        return selectedButton;
     }
 
-
-    void CatdRotate(Transform _BUTTONTR,string _FILENAME, string _FCARDNAME, string _BCARDNAME)
-    {
-
-        Sprite _frontCard = Resources.Load<Sprite>($"03_Source/{_FILENAME}/{_FCARDNAME}");
-        Sprite _backCard = Resources.Load<Sprite>($"03_Source/04_back/{_BCARDNAME}");
-
-        cardRenderer.sprite = _backCard;
-
-        var _sequence = DOTween.Sequence();
-        _sequence.Append(_BUTTONTR.DORotate(_BUTTONTR.eulerAngles
-            + new Vector3(0, 90, 0), 0.5f)).SetEase(ease);
-
-        _sequence.AppendCallback(() =>
-        { cardRenderer.sprite = (_BUTTONTR.eulerAngles.y < 180) ? cardRenderer.sprite = _frontCard : cardRenderer.sprite = _backCard; });
-
-        _sequence.Append(_BUTTONTR.DORotate(_BUTTONTR.eulerAngles
-           + new Vector3(0, 360, 0), 0.6f)).SetEase(ease);
-
-        if(_BUTTONTR.eulerAngles.y <= 360)
+     
+        void SetFalseCard(GameObject _CANVAS1, GameObject _CANVAS2, GameObject _CANVAS3)
         {
-            StartCoroutine(CardAssemble(selectedCardPos, 1f));
+            _CANVAS1.SetActive(true);
+            _CANVAS2.SetActive(false);
+            _CANVAS3.SetActive(false);
+
+            cards.Clear();
+
+            SetCard(_CANVAS1);
         }
-    }
+
+        void CardRotate(Transform _BUTTONTR, string _FILENAME, string _FCARDNAME, string _BCARDNAME, GameObject _CANVAS1, GameObject _CANVAS2, GameObject _CANVAS3)//GameObject _CANVAS1, GameObject _CANVAS2, GameObject _CANVAS3, Button _BUTTON01, Button[] _BUTTONS
+        {
+
+
+
+            Sprite _frontCard = Resources.Load<Sprite>($"03_Source/{_FILENAME}/{_FCARDNAME}");
+            Sprite _backCard = Resources.Load<Sprite>($"03_Source/04_back/{_BCARDNAME}");
+
+            selectedButton.image.sprite = _backCard;
+
+            var _sequence = DOTween.Sequence();
+            _sequence.Append(_BUTTONTR.DORotate(_BUTTONTR.eulerAngles
+                + new Vector3(0, 90, 0), 0.5f)).SetEase(ease);
+
+            _sequence.AppendCallback(() =>
+            { selectedButton.image.sprite = (_BUTTONTR.eulerAngles.y < 180) ? selectedButton.image.sprite = _frontCard : selectedButton.image.sprite = _backCard; });
+
+            _sequence.Append(_BUTTONTR.DORotate(_BUTTONTR.eulerAngles
+               + new Vector3(0, 360, 0), 0.6f)).SetEase(ease);
+
+            CardChage(_CANVAS1, _CANVAS2, _CANVAS3);
+
+        }
+
+        void CardChage(GameObject _CANVAS1, GameObject _CANVAS2, GameObject _CANVAS3)
+        {
+            DOVirtual.DelayedCall(1f, () => ReturnAllCards());
+            DOVirtual.DelayedCall(3f, () => SetFalseCard(_CANVAS1, _CANVAS2, _CANVAS3));
+        }
     
-
-
 }
+
+
+
+
+//Button SelectButton(GameObject _PARENTCANVAS, Button[] _ALLBUTTONS)
+//{
+//    _ALLBUTTONS = _PARENTCANVAS.GetComponentsInChildren<Button>();
+
+//    if (_ALLBUTTONS == null || _ALLBUTTONS.Length == 0)
+//    {
+//        Debug.LogWarning($"{_PARENTCANVAS.name} ÏïàÏóê ButtonÏù¥ ÏóÜÏäµÎãàÎã§!");
+//        return null;
+//    }
+
+//    int _buttonIndex = UnityEngine.Random.Range(0, _ALLBUTTONS.Length);
+//    selectedButton = _ALLBUTTONS[_buttonIndex];
+
+//    for (int i = 0; i < _ALLBUTTONS.Length; i++)
+//    {
+//        if (i == _buttonIndex)
+//        {
+//            _ALLBUTTONS[i].interactable = true; // ÌÅ¥Î¶≠ Í∞ÄÎä•
+//        }
+//        else
+//        {
+//            _ALLBUTTONS[i].interactable = false;
+//        }
+//    }
+
+//    cardRenderer = selectedButton.GetComponent<Image>();
+
+
+
+//    return selectedButton;
+//}
--- Assets/02_Scripts/SpreadCard.cs
+++ Assets/02_Scripts/SpreadCard.cs
@@ -20,6 +20,19 @@ public class SpreadCard : MonoBehaviour
 
     public List<Transform> cards = new List<Transform>();
 
+
+
+    void Start()
+    {
+
+    }
+
+
+    public void ReturnAllCards()
+    {
+        StartCoroutine(ReturnCards());
+    }
+
     protected void SetCard(GameObject _PARENTPOS)
     {
 
@@ -101,38 +114,109 @@ public class SpreadCard : MonoBehaviour
         _CARD.localScale = _endScale;
     }
 
-
-    protected IEnumerator CardAssemble(Vector3 _TARGETPOS, float _DURATION)
+    IEnumerator ReturnCards()
     {
-        _cardCount = cards.Count;
-
-        for (int i = _cardCount; i > 0; i--)
+        for (int i = 0; i < cards.Count; i++)
         {
+            RectTransform card = cards[i].GetComponent<RectTransform>();
+            Vector2 originalPos = new Vector2(card.anchoredPosition.x, card.anchoredPosition.y);
+            //((cards[0].GetComponent<RectTransform>();
 
-            Vector3 _startPos = new Vector3(card.anchoredPosition.x, card.anchoredPosition.y);
-            Vector3 _startScale = Vector3.one;
-            Vector3 _endScale = Vector3.zero;
+            // 카드 되돌리는 애니메이션
+            StartCoroutine(AnimateCardBack(card, originalPos, moveDuration));
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
 
-            float t = 0f;
-            while (t < _DURATION)
-            {
-                t += Time.deltaTime;
-                float _progress = Mathf.SmoothStep(0, 1, t / _DURATION);
-                card.anchoredPosition = Vector3.Lerp(_startPos, _TARGETPOS, _progress);
-                card.localScale = Vector3.Lerp(_startScale, _endScale, _progress);
-                yield return null;
+    IEnumerator AnimateCardBack(RectTransform _CARD, Vector3 _TARGETPOS, float _DURATION)
+    {
+        Vector3 _startPos = _CARD.anchoredPosition;
+        Vector3 _startScale = _CARD.localScale;
+        Vector3 _endScale = Vector3.zero;
 
-            }
-
-            card.localPosition = _TARGETPOS;
-            card.localScale = _endScale;
-            cards.RemoveAt(i);
-            
-
+        float t = 0f;
+        while (t < _DURATION)
+        {
+            t += Time.deltaTime;
+            float _progress = Mathf.SmoothStep(0, 1, t / _DURATION);
+            _CARD.anchoredPosition = Vector3.Lerp(_startPos, _TARGETPOS, _progress);
+            _CARD.localScale = Vector3.Lerp(_startScale, _endScale, _progress);
             yield return null;
         }
 
+        _CARD.anchoredPosition = _TARGETPOS;
+        _CARD.localScale = _endScale;
     }
-
-
 }
+
+    //public IEnumerator AssembleCards()
+    //{
+    //    if (startPos == null)
+    //    {
+    //        Debug.LogWarning("StartPoint가 설정되지 않았습니다!");
+    //        yield break;
+    //    }
+
+    //   RectTransform _startPos = startPos.GetComponent<RectTransform>();
+
+
+    //    int _cardIndex = cards.Count;
+
+    //    for (int y = 0; y < rows; y--)
+    //    {
+    //        for (int x = 0; x < columns; x--)
+    //        {
+    //            if (_cardIndex <= cards.Count)
+    //            {
+    //                yield break;
+    //            }
+
+    //            card = cards[_cardIndex].GetComponent<RectTransform>();
+
+
+    //            yield return new WaitForSeconds(0.2f);
+
+    //            Vector2 _targetPos = new Vector2(
+    //                startPos.anchoredPosition.x ,
+    //                startPos.anchoredPosition.y
+    //            );
+
+    //            StartCoroutine(CardAssemble(_targetPos, moveDuration));
+    //            _cardIndex--;
+    //            Debug.Log(_cardIndex);
+    //        }
+    //    }
+    //}
+
+    //protected IEnumerator CardAssemble(Vector3 _TARGETPOS, float _DURATION)
+    //{
+    //    _cardCount = cards.Count;
+
+    //    for (int i = _cardCount; i > 0; i--)
+    //    {
+
+    //        Vector3 _startPos = new Vector3(card.anchoredPosition.x, card.anchoredPosition.y);
+    //        Vector3 _startScale = Vector3.one;
+    //        Vector3 _endScale = Vector3.zero;
+
+    //        float t = 0f;
+    //        while (t < _DURATION)
+    //        {
+    //            t += Time.deltaTime;
+    //            float _progress = Mathf.SmoothStep(0, 1, t / _DURATION);
+    //            card.anchoredPosition = Vector3.Lerp(_startPos, _TARGETPOS, _progress);
+    //            card.localScale = Vector3.Lerp(_startScale, _endScale, _progress);
+    //            yield return null;
+
+    //        }
+
+    //        card.localPosition = _TARGETPOS;
+    //        card.localScale = _endScale;
+    //        cards.RemoveAt(i);
+            
+
+    //        yield return null;
+    //    }
+
+    //}
+
