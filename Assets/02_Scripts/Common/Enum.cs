a59563554f4d19db2ef2a10d1a785bdf551c2fc3
00CHI <suinmoon83@gmail.com>
2025-11-09 01:03:56 +0900

    251109_AM1:03
    - AI스테이트 작업중
    - 카드정보저장 작업중
    
    **도형별 카드 빼기
    **인원 수대로 방생성되도록하기
    ** 게임시작 => 카드 선택
    카드저장=> AI 카드 선택
    
    ** 나왔던 질문 안나오게 구현
    <= 인덱스 문제 해결 필요
    ** 질문카드 뒤집기 애니메이션
    ** 학생부장 죄,시간 카드 선택 안되도록 수정

---
+++ Assets/02_Scripts/Common/Enum.cs
@@ -0,0 +1,84 @@
+public enum eAISTATE
+{
+    eAISTATE_NONE,
+    eAISTATE_WAIT,
+    eAISTATE_QUESTION,
+    eAISTATE_ANSWER,
+    eAISTATE_REASONING
+
+}
+
+public enum eCHARACTER
+{
+    eCHARACTER_NONE,
+    eCHARACTER_PLAYER,
+    eCHARACTER_AI
+
+}
+public enum eSTAT
+{
+    eSTAT_NONE,
+    eSTAT_STUDENT,
+    eSTAT_TEACHER,
+    eSTAT_TIME,
+    eSTAT_CRIME
+}
+
+public enum eSTUDENT
+{
+    eSTUDENT_NONE,
+    eSTUDENT_Ari_Choi,///1
+    eSTUDENT_bada_Seo,//2
+    eSTUDENT_Bora_Nam,//3
+    eSTUDENT_doha_Bae,//4
+    eSTUDENT_Donghoon_Moon,//5
+    eSTUDENT_galam_Heo,//6
+    eSTUDENT_hali_Gu,//7
+    eSTUDENT_hanbyeol_Bu,//8
+    eSTUDENT_Jiho_Lee,//9
+    eSTUDENT_mingug_Jo,//10
+    eSTUDENT_Minjae_Kim,//11
+    eSTUDENT_Mirae_Yoon,//12
+    eSTUDENT_schoolmaste//13
+}
+public enum eTIME
+{
+    eTIME_NONE,
+    eTIME_After,//1
+    eTIME_Break,//2
+    eTIME_CleaningTime,//3
+    eTIME_DropOff,//4
+    eTIME_GoToSchool,//5
+    eTIME_Lunch,//6
+}
+
+public enum eCRIME
+{
+    //"Absence",//1
+    //"Alcohol",//2
+    //"Cigarette",//3
+    //"Cleaning",//4
+    //"Doodle",//5
+    //"Dues",//6
+    //"Homework",//7
+    //"InformalLanguage",//8
+    //"Restroom",//9
+    //"Teacher",//10
+    //"Test",//11
+    //"Washing",//12
+
+    eCRIME_NONE,
+    eCRIME_Absence,//1
+    eCRIME_Alcohol,//2
+    eCRIME_Cigarette,//3
+    eCRIME_Cleaning,//4
+    eCRIME_Doodle,//5
+    eCRIME_Dues,//6
+    eCRIME_Homework,//7
+    eCRIME_InformalLanguage,//8
+    eCRIME_Restroom,//9       
+    eCRIME_Teacher,//10
+    eCRIME_Test,//11
+    eCRIME_Washing,//12
+
+}
---
+++ Assets/02_Scripts/Common/Singleton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+public static class Singleton
+{
+    public static GameManager GameManager;
+    public static SpriteManager SpriteManager;
+
+    public static RoomManager RoomManager;
+
+    public static SelectCard SelectCard;
+    public static RandomQuestion RandomQuestion;
+
+    //public static AudioManager AudioManager;
+}
