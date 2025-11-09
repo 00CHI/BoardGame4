df3c57d471b8975bca4ac04e8536d6dba22ab3d5
00CHI <suinmoon83@gmail.com>
2025-11-09 20:43:21 +0900

    Revert "251109_PM8:43"
    This reverts commit a464bde4cde7463751e9552434d3efd589d71101.

---
+++ Assets/02_Scripts/Common/Singleton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+public static class Singleton
+{
+    public static GameManager GameManager;
+    public static SpriteManager SpriteManager;
+
+    public static RoomManager RoomManager;
+
+    public static Player Player;
+    public static AI AI;
+
+    public static SelectCard SelectCard;
+    public static RandomQuestion RandomQuestion;
+
+    //public static AudioManager AudioManager;
+}
